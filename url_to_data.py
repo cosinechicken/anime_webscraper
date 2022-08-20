10fea6ee7b514758f7b791b3f1ab696f4be7f540
cosinechicken <ah.blenden@gmail.com>
2022-08-20 17:57:49 -0400

    Added 20160120 support
    Also commented and cleaned the code

--- url_to_data.py
+++ url_to_data.py
@@ -20,6 +20,11 @@ class bcolors:
 
 # remove commas from string representation of number
 def remove_commas(num):
+    while num[0] == '\t' or num[0] == '\n' or num[0] == ' ':
+        num = num[1:]
+    while num[-1] == '\t' or num[-1] == '\n' or num[-1] == ' ':
+        num = num[:-1]
+
     arr = num.split(",")
     new_str = ""
     for chunk in arr:
@@ -54,15 +59,32 @@ for next_url in urls:
 
     soup = BeautifulSoup(htmlSource, 'lxml')    # Enable us to search the scraped HTML easily
     try:
-        rows = soup.find_all('td', class_="borderClass", align="left", valign="top")
-        data = []
-        for row in rows:
-            name = str(row.contents[3].strong.string)
-            score = str(row.contents[7].contents[0]).split("scored ")[1]
-            members = remove_commas(str(row.contents[7].span.string).split(" ")[0])
-            data.append(name + ", " + score + ", " + members)
-            
+        # Turn the date into a string
         date_str = next_url.split("web/")[1].split("/")[0]
+        data = []
+        if int(date_str) < 20160120:
+            rows = soup.find_all('td', class_="borderClass", align="left", valign="top")
+            # Collect information about name, score, and members from each row
+            for row in rows:
+                name = str(row.contents[3].strong.string)
+                score = str(row.contents[7].contents[0]).split("scored ")[1]
+                members = remove_commas(str(row.contents[7].span.string).split(" ")[0])
+                # Append the data to the array
+                data.append(name + ", " + score + ", " + members)
+        else:
+            rows = soup.find_all('tr', class_="ranking-list")
+            # Collect information about name, score, and members from each row
+            for row in rows:
+                name = str(row.contents[3].contents[3].contents[3].string)
+                score = str(row.contents[5].div.span.string)
+                members = remove_commas(str(row.contents[3].contents[3].contents[6].contents[4])).split(" members")[0]
+                # Append the data to the array
+                data.append(name + ", " + score + ", " + members)
+
+        # If length was 0 then there was an error in scraping the files
+        if len(data) == 0:
+            raise Exception("File was empty")
+        # If everything went well we write the data into the text files
         with open('data/' + date_str + '.txt', 'w', encoding='utf8') as txt_file:
             for data_point in data:
                 txt_file.write(data_point + "\n")
