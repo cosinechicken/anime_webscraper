f2e3ff126063f510bb2d23698d0cbcba3cfbe721
cosinechicken <ah.blenden@gmail.com>
2022-08-21 15:35:22 -0400

    Add failure catching

--- url_to_data.py
+++ url_to_data.py
@@ -44,6 +44,7 @@ op = webdriver.ChromeOptions()
 op.add_argument('headless')
 
 urls = []
+failures = []
 with open('data/urls.txt', 'r') as url_file:
     while True:
         next_url = url_file.readline()
@@ -60,67 +61,77 @@ for next_url in urls:
     # Scrape the url
     driver = webdriver.Chrome(options=op)
     driver.set_page_load_timeout(60)
-    try:
-        driver.get(next_url)
-    except:
-        print(bcolors.FAIL + "OUT OF TIME" + bcolors.ENDC)
-    # Sleep for 2 seconds to allow javascript to render
-    time.sleep(2)
-    htmlSource = driver.page_source         # Scraped HTML of the website
+    is_finished = False
+    for i in range(3):
+        try:
+            driver.get(next_url)
+        except:
+            print(bcolors.FAIL + "OUT OF TIME" + bcolors.ENDC)
+        # Sleep for 2 seconds to allow javascript to render
+        time.sleep(2)
+        htmlSource = driver.page_source         # Scraped HTML of the website
 
-    soup = BeautifulSoup(htmlSource, 'lxml')    # Enable us to search the scraped HTML easily
-    try:
-        # Turn the date into a string
-        date_str = next_url.split("web/")[1].split("/")[0]
-        data = []
-        if int(date_str) < 20160120:
-            rows = soup.find_all('td', class_="borderClass", align="left", valign="top")
-            # Collect information about name, score, and members from each row
-            for row in rows:
-                name = str(row.contents[3].strong.string)
-                score = str(row.contents[7].contents[0]).split("scored ")[1]
-                members = remove_commas(str(row.contents[7].span.string).split(" ")[0])
-                # Append the data to the array
-                add_data(data, name, score, members)
-                
-        elif int(date_str) < 20160406:
-            rows = soup.find_all('tr', class_="ranking-list")
-            # Collect information about name, score, and members from each row
-            for row in rows:
-                name = str(row.contents[3].contents[3].contents[3].string)
-                score = str(row.contents[5].div.span.string)
-                members = remove_commas(str(row.contents[3].contents[3].contents[6].contents[4])).split(" ")[0]
-                # Append the data to the array
-                add_data(data, name, score, members)
-        elif int(date_str) < 20160504:
-            rows = soup.find_all('tr', class_="ranking-list")
-            # Collect information about name, score, and members from each row
-            for row in rows:
-                name = str(row.contents[3].contents[3].contents[2].string)
-                score = str(row.contents[5].div.span.string)
-                members = remove_commas(str(row.contents[3].contents[3].contents[-1].contents[-1])).split(" ")[0]
-                # Append the data to the array
-                add_data(data, name, score, members)
-        else:
-            rows = soup.find_all('tr', class_="ranking-list")
-            # Collect information about name, score, and members from each row
-            for row in rows:
-                name = str(row.contents[3].contents[3].contents[2].a.string)
-                score = str(row.contents[5].div.span.string)
-                members = remove_commas(str(row.contents[3].contents[3].contents[4].contents[-1])).split(" ")[0]
-                # Append the data to the array
-                add_data(data, name, score, members)
+        soup = BeautifulSoup(htmlSource, 'lxml')    # Enable us to search the scraped HTML easily
+    
+        try:
+            # Turn the date into a string
+            date_str = next_url.split("web/")[1].split("/")[0]
+            data = []
+            if int(date_str) < 20160120:
+                rows = soup.find_all('td', class_="borderClass", align="left", valign="top")
+                # Collect information about name, score, and members from each row
+                for row in rows:
+                    name = str(row.contents[3].strong.string)
+                    score = str(row.contents[7].contents[0]).split("scored ")[1]
+                    members = remove_commas(str(row.contents[7].span.string).split(" ")[0])
+                    # Append the data to the array
+                    add_data(data, name, score, members)
+                    
+            elif int(date_str) < 20160406:
+                rows = soup.find_all('tr', class_="ranking-list")
+                # Collect information about name, score, and members from each row
+                for row in rows:
+                    name = str(row.contents[3].contents[3].contents[3].string)
+                    score = str(row.contents[5].div.span.string)
+                    members = remove_commas(str(row.contents[3].contents[3].contents[6].contents[4])).split(" ")[0]
+                    # Append the data to the array
+                    add_data(data, name, score, members)
+            elif int(date_str) < 20160504:
+                rows = soup.find_all('tr', class_="ranking-list")
+                # Collect information about name, score, and members from each row
+                for row in rows:
+                    name = str(row.contents[3].contents[3].contents[2].string)
+                    score = str(row.contents[5].div.span.string)
+                    members = remove_commas(str(row.contents[3].contents[3].contents[-1].contents[-1])).split(" ")[0]
+                    # Append the data to the array
+                    add_data(data, name, score, members)
+            else:
+                rows = soup.find_all('tr', class_="ranking-list")
+                # Collect information about name, score, and members from each row
+                for row in rows:
+                    name = str(row.contents[3].contents[3].contents[2].a.string)
+                    score = str(row.contents[5].div.span.string)
+                    members = remove_commas(str(row.contents[3].contents[3].contents[4].contents[-1])).split(" ")[0]
+                    # Append the data to the array
+                    add_data(data, name, score, members)
 
-        # If length was 0 then there was an error in scraping the files
-        if len(data) == 0:
-            raise Exception("File was empty")
-        elif len(data) != 50:
-            raise Exception("File did not have 50 datapoints")
-        # If everything went well we write the data into the text files
-        with open('data/' + date_str + '.txt', 'w', encoding='utf8') as txt_file:
-            for data_point in data:
-                txt_file.write(data_point + "\n")
-    except Exception as e:
-        print(next_url + " doesn't have the correct format.")
-        print(bcolors.WARNING + "Scraping: " + str(e) + bcolors.ENDC)
-        break
+            # If length was 0 then there was an error in scraping the files
+            if len(data) == 0:
+                raise Exception("File was empty")
+            elif len(data) != 50:
+                raise Exception("File did not have 50 datapoints")
+            # If everything went well we write the data into the text files
+            with open('data/' + date_str + '.txt', 'w', encoding='utf8') as txt_file:
+                for data_point in data:
+                    txt_file.write(data_point + "\n")
+            is_finished = True
+            break
+        except Exception as e:
+            print(next_url + " doesn't have the correct format.")
+            print(bcolors.WARNING + "Scraping: " + str(e) + bcolors.ENDC)
+    if not is_finished:
+        with open('data/failures.txt', 'a', encoding='utf8') as txt_file:
+            txt_file.write(str(date_str) + "\n")
+        failures.append(date_str)
+        if len(failures) > 10:
+            break
